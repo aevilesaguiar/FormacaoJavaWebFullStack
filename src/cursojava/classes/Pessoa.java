6bf3a772ccdc52c72e39861576334d1cbc01f29a
aevilesaguiar <aeviles@hotmail.com>
2022-01-14 19:10:45 -0300

    first commit

---
+++ src/cursojava/classes/Pessoa.java
@@ -0,0 +1,12 @@
+package cursojava.classes;
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+
+}
